54ecf037ff4250146b0e8572bb878e2a735074b0
Mazidul <md.mazidulhasan1@gmail.com>
2024-03-27 03:31:54 +0600

    Locator design with single driver object

---
+++ src/test/java/com/seleniumwithjavanavpractice/Locator/OpenCartLoginTest.java
@@ -0,0 +1,27 @@
+package com.seleniumwithjavanavpractice.Locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.seleniumwithjavanavpractice.BrowserUtil;
+
+public class OpenCartLoginTest {
+    public static void main(String[] args) {
+        BrowserUtil browserUtil = new BrowserUtil();
+        WebDriver driver = browserUtil.launchBrowser("chrome");
+        
+        browserUtil.launchURL("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+
+        String title = browserUtil.getPageTitle();
+        System.out.println(title);
+
+        By emailBy = By.id("input-email");
+        By passBy = By.id("input-password");
+
+        ElementUtil elementUtil = new ElementUtil(driver);
+        elementUtil.doSenKeys(emailBy, "test@gmai.com");
+        elementUtil.doSenKeys(passBy, "test123");
+
+        // browserUtil.closeBrowser();
+    }
+}
