30a3f8e902e9d7b3f135ba60b17b06fe17f21b84
Mazidul <md.mazidulhasan1@gmail.com>
2024-04-22 23:44:30 +0600

    Working with custom xpath

---
+++ src/test/java/com/seleniumwithjavanavpractice/Locator/CustomXpath.java
@@ -0,0 +1,46 @@
+package com.seleniumwithjavanavpractice.Locator;
+
+public class CustomXpath {
+    public static void main(String[] args) {
+
+        //1. using single attribure 
+        //tagnaem[@attriName='value']
+        // example
+        //input[@name]
+        //input[@name='Fname']
+
+        // 2. Using multiple attribure
+        //tagnaem[@attriName='value' and @att2='value2']
+        //tagnaem[@attriName='value' or @att2='value2']
+
+        //tagnaem[@attri1 or @att2='value2']
+        // this is also valid, it is saying that i need an attribute called attri1 
+        // and also another attribute called att2 but i know the value of att2
+
+        //3. text()
+        // tagnaem[text()='value']
+        //a[text()='Contact Us ']
+
+        // 4. text() and attribute
+        //tagname[@attr='value' and text()='value']
+
+        // 5 contains() with attr:
+        // tagname[contains(@arrName,'value')]
+        //input[contains(@placeholder,'E-Mail Address')]
+
+        // tagname[contains(@arrName1,'value') and @attr2='value']
+        //input[contains(@placeholder,'E-Mail Address') and @name='email'] 
+
+        // 6. contains() with text():
+        // tagname[contains(text(),'value')]
+
+        // 7. contains() with text() and attr:
+        //tagname[contains(text(),'value') and contains(@attr,'value')]
+        //a[contains(text(),'Notebooks') and contains(@href,'category&path')]
+
+        //8. starts with():
+        //tagname[starts-with(@att,'value')]
+        //input[starts-with(@placeholder,'E-Mail')]
+
+    }
+}
