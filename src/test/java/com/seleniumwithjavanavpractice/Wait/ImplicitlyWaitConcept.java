e1353ad609136448cd978154d2a7f5cd7586844c
Mazidul <md.mazidulhasan1@gmail.com>
2024-08-22 00:30:28 +0600

    Wait concepts, implicit wait

---
+++ src/test/java/com/seleniumwithjavanavpractice/Wait/ImplicitlyWaitConcept.java
@@ -0,0 +1,36 @@
+package com.seleniumwithjavanavpractice.Wait;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitlyWaitConcept {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // sel 4.x
+        // implecitly wait :: Global wait: Applied to all the elements by default
+        // it will wait for an elemet 10s max for an element
+        // if the element is found before 10 sec then it will take action and go to the
+        // next thing
+
+        // so in any place i use this driver it will wait for that max time
+        // if there are multiple class, and if i use this driver then it will beahave same
+
+        // case study 1
+        // what if home page is very slow and we want to wait longer like 15s
+        // then we need to define the same thing again by over writer
+        // so after this it will again work for recent updated time 15s
+        // in another page if the page takes less time and want to configure it
+        // we need to overright it again
+        // so it becomes a problem
+
+        // case study 2
+        // login page : 10s time out
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Test");
+
+    }
+}
---
+++ src/test/java/com/seleniumwithjavanavpractice/Wait/WaitConcept.java
@@ -0,0 +1,18 @@
+package com.seleniumwithjavanavpractice.Wait;
+
+public class WaitConcept {
+    public static void main(String[] args) {
+        // wait 2 types
+        // 1. Static wait :: Thread.sleep()
+        // 2. Dynamic wait :: 
+                //a. Implicit wait
+                //b. Explicit wait
+                    // i: WebDriverWait (class)
+                    //ii: FluentWait (class)
+        // There is wait interface
+            // until() --> has a child class fluentwait
+            // so it is has some extrac methods 
+            // finally it generates webdriverWait
+
+    }
+}
