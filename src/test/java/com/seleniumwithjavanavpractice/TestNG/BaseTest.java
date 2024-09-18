07f8c4d9d601a4abde45825ca43cdc293c0af89d
Mazidul <md.mazidulhasan1@gmail.com>
2024-09-18 22:39:00 +0600

    TestNG updated with testng.xml

--- src/test/java/com/seleniumwithjavanavpractice/TestNG/BaseTest.java
+++ src/test/java/com/seleniumwithjavanavpractice/TestNG/BaseTest.java
@@ -4,15 +4,49 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import com.seleniumwithjavanavpractice.BasicWebScrapingConcepts.EleException;
 
 public class BaseTest {
     WebDriver driver;
 
+    @Parameters({"url", "browser", "headless"})
     @BeforeTest
-    public void setup(){
-        driver = new ChromeDriver();
+    public void setup(String url, String browser, String headless){
+        switch (browser.toLowerCase().trim()) {
+            case "chrome":
+                ChromeOptions co = new ChromeOptions();
+                if (Boolean.parseBoolean(headless)) {
+                    co.addArguments("--headless");
+                }
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                FirefoxOptions fo = new FirefoxOptions();
+                if (Boolean.parseBoolean(headless)) {
+                    fo.addArguments("--headless");
+                }
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                EdgeOptions eo = new EdgeOptions();
+                if (Boolean.parseBoolean(headless)) {
+                    eo.addArguments("--headless");
+                }
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("Please write the proper browser name instead of : "+ browser);
+                throw new EleException("Browser Not found");
+            }
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
--- src/test/java/com/seleniumwithjavanavpractice/TestNG/GoogleTest.java
+++ src/test/java/com/seleniumwithjavanavpractice/TestNG/GoogleTest.java
@@ -1,13 +1,35 @@
 package com.seleniumwithjavanavpractice.TestNG;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.seleniumwithjavanavpractice.BasicWebScrapingConcepts.EleException;
+
 public class GoogleTest extends BaseTest{
+
+    @Parameters({"url", "browser"})
     @Test(priority = 1)
-    public void titleTest(){
-        driver.get("https://www.google.com/");
+    public void titleTest(String url, String browser){
+        switch (browser.toLowerCase().trim()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("Please write the proper browser name instead of : "+ browser);
+                throw new EleException("Browser Not found");
+        }
+        driver.get(url);
         String title = driver.getTitle();
         System.out.println(title);
         Assert.assertEquals(title, "Google", "Title did not match");
